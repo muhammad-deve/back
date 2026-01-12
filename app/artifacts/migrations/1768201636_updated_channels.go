bc361306c818a67a42caec236162f4c6bb918523
Muhammad <muhammadjonxudaynazarov226@gmail.com>
2026-01-12 12:09:00 +0500

    Deleted PB

---
+++ app/artifacts/migrations/1768201636_updated_channels.go
@@ -0,0 +1,124 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3009067695")
+		if err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("relation2092043024")
+
+		// remove field
+		collection.Fields.RemoveById("relation1400097126")
+
+		// remove field
+		collection.Fields.RemoveById("relation3571151285")
+
+		// remove field
+		collection.Fields.RemoveById("relation3834550803")
+
+		// remove field
+		collection.Fields.RemoveById("relation105650625")
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3009067695")
+		if err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(4, []byte(`{
+			"cascadeDelete": false,
+			"collectionId": "pbc_893542168",
+			"hidden": false,
+			"id": "relation2092043024",
+			"maxSelect": 1,
+			"minSelect": 0,
+			"name": "quality",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "relation"
+		}`)); err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(5, []byte(`{
+			"cascadeDelete": false,
+			"collectionId": "pbc_961350965",
+			"hidden": false,
+			"id": "relation1400097126",
+			"maxSelect": 1,
+			"minSelect": 0,
+			"name": "country",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "relation"
+		}`)); err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(6, []byte(`{
+			"cascadeDelete": false,
+			"collectionId": "pbc_3304764897",
+			"hidden": false,
+			"id": "relation3571151285",
+			"maxSelect": 1,
+			"minSelect": 0,
+			"name": "language",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "relation"
+		}`)); err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(7, []byte(`{
+			"cascadeDelete": false,
+			"collectionId": "pbc_1410514596",
+			"hidden": false,
+			"id": "relation3834550803",
+			"maxSelect": 1,
+			"minSelect": 0,
+			"name": "logo",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "relation"
+		}`)); err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(8, []byte(`{
+			"cascadeDelete": false,
+			"collectionId": "pbc_3292755704",
+			"hidden": false,
+			"id": "relation105650625",
+			"maxSelect": 1,
+			"minSelect": 0,
+			"name": "category",
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "relation"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
---
+++ app/artifacts/migrations/1768201684_deleted_contents.go
@@ -0,0 +1,245 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_4105455982")
+		if err != nil {
+			return err
+		}
+
+		return app.Delete(collection)
+	}, func(app core.App) error {
+		jsonData := `{
+			"createRule": null,
+			"deleteRule": null,
+			"fields": [
+				{
+					"autogeneratePattern": "[a-z0-9]{15}",
+					"hidden": false,
+					"id": "text3208210256",
+					"max": 15,
+					"min": 15,
+					"name": "id",
+					"pattern": "^[a-z0-9]+$",
+					"presentable": false,
+					"primaryKey": true,
+					"required": true,
+					"system": true,
+					"type": "text"
+				},
+				{
+					"autogeneratePattern": "",
+					"hidden": false,
+					"id": "text1404385515",
+					"max": 0,
+					"min": 0,
+					"name": "imdb_id",
+					"pattern": "",
+					"presentable": false,
+					"primaryKey": false,
+					"required": false,
+					"system": false,
+					"type": "text"
+				},
+				{
+					"autogeneratePattern": "",
+					"hidden": false,
+					"id": "text1438434789",
+					"max": 0,
+					"min": 0,
+					"name": "tmdb_id",
+					"pattern": "",
+					"presentable": false,
+					"primaryKey": false,
+					"required": false,
+					"system": false,
+					"type": "text"
+				},
+				{
+					"autogeneratePattern": "",
+					"hidden": false,
+					"id": "text724990059",
+					"max": 0,
+					"min": 0,
+					"name": "title",
+					"pattern": "",
+					"presentable": false,
+					"primaryKey": false,
+					"required": false,
+					"system": false,
+					"type": "text"
+				},
+				{
+					"hidden": false,
+					"id": "select2363381545",
+					"maxSelect": 2,
+					"name": "type",
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "select",
+					"values": [
+						"movie",
+						"serie"
+					]
+				},
+				{
+					"hidden": false,
+					"id": "number4134912800",
+					"max": null,
+					"min": null,
+					"name": "release_year",
+					"onlyInt": false,
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "number"
+				},
+				{
+					"hidden": false,
+					"id": "number2254405824",
+					"max": null,
+					"min": null,
+					"name": "duration",
+					"onlyInt": false,
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "number"
+				},
+				{
+					"autogeneratePattern": "",
+					"hidden": false,
+					"id": "text3199963017",
+					"max": 0,
+					"min": 0,
+					"name": "plot",
+					"pattern": "",
+					"presentable": false,
+					"primaryKey": false,
+					"required": false,
+					"system": false,
+					"type": "text"
+				},
+				{
+					"hidden": false,
+					"id": "number1267793696",
+					"max": null,
+					"min": null,
+					"name": "imdb_rating",
+					"onlyInt": false,
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "number"
+				},
+				{
+					"hidden": false,
+					"id": "number3568314025",
+					"max": null,
+					"min": null,
+					"name": "vote_count",
+					"onlyInt": false,
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "number"
+				},
+				{
+					"autogeneratePattern": "",
+					"hidden": false,
+					"id": "text2092043024",
+					"max": 0,
+					"min": 0,
+					"name": "quality",
+					"pattern": "",
+					"presentable": false,
+					"primaryKey": false,
+					"required": false,
+					"system": false,
+					"type": "text"
+				},
+				{
+					"autogeneratePattern": "",
+					"hidden": false,
+					"id": "text4019417054",
+					"max": 0,
+					"min": 0,
+					"name": "poster_url",
+					"pattern": "",
+					"presentable": false,
+					"primaryKey": false,
+					"required": false,
+					"system": false,
+					"type": "text"
+				},
+				{
+					"hidden": false,
+					"id": "number2023681770",
+					"max": null,
+					"min": null,
+					"name": "poster_width",
+					"onlyInt": false,
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "number"
+				},
+				{
+					"hidden": false,
+					"id": "number515331995",
+					"max": null,
+					"min": null,
+					"name": "poster_height",
+					"onlyInt": false,
+					"presentable": false,
+					"required": false,
+					"system": false,
+					"type": "number"
+				},
+				{
+					"hidden": false,
+					"id": "autodate2990389176",
+					"name": "created",
+					"onCreate": true,
+					"onUpdate": false,
+					"presentable": false,
+					"system": false,
+					"type": "autodate"
+				},
+				{
+					"hidden": false,
+					"id": "autodate3332085495",
+					"name": "updated",
+					"onCreate": true,
+					"onUpdate": true,
+					"presentable": false,
+					"system": false,
+					"type": "autodate"
+				}
+			],
+			"id": "pbc_4105455982",
+			"indexes": [],
+			"listRule": null,
+			"name": "contents",
+			"system": false,
+			"type": "base",
+			"updateRule": null,
+			"viewRule": null
+		}`
+
+		collection := &core.Collection{}
+		if err := json.Unmarshal([]byte(jsonData), &collection); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
